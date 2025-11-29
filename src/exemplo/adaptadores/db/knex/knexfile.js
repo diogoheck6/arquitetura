e54ce58c4b667461edff5760b2f6914dfe8f79ec
diogoheck6 <diogoheck6@gmail.com>
2025-11-29 17:21:41 -0300

    knex atualizacao

--- src/exemplo/adaptadores/db/knex/knexfile.js
+++ src/exemplo/adaptadores/db/knex/knexfile.js
@@ -7,8 +7,6 @@ dotenv.config({
 	path: path.resolve(__dirname, '../../../../../.env'),
 })
 
-console.log('DB_URL no knexfile:', process.env.DB_URL)
-
 module.exports = {
 	client: 'pg',
 	connection: process.env.DB_URL,
